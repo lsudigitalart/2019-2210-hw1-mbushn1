0521344061d4a13e80d61ce17ace2002e12d064b
mbushn1 <46574392+mbushn1@users.noreply.github.com>
2019-08-29 13:52:38 -0500

    first assignment commit

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+ createCanvas(1500,800);
+ background(255,255,255);
+ ellipse(width/2,height/2,600,600);
+ ellipse(750,400,50,50);
+ ellipse(750,450,60,60);
+ ellipse(750,500,70,70);
+ ellipse(740,395,5,5);
+ ellipse(760,395,5,5);
+ ellipse(750,410,10,10);
+ 
+
+
+
+
+
+}
+
+function draw() {
+
+}

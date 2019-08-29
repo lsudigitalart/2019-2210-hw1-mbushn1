3f7285ff65525c14e675b759b4b03af3aca5097a
mbushn1 <46574392+mbushn1@users.noreply.github.com>
2019-08-29 14:43:39 -0500

    8/29

--- sketch.js
+++ sketch.js
@@ -1,19 +1,24 @@
 function setup() {
  createCanvas(1500,800);
- background(255,255,255);
+ background(0,0,0);
+//snowglobe 
  ellipse(width/2,height/2,600,600);
+//snowman
  ellipse(750,400,50,50);
- ellipse(750,450,60,60);
- ellipse(750,500,70,70);
+ ellipse(750,455,70,70);
+ ellipse(750,520,90,90);
  ellipse(740,395,5,5);
  ellipse(760,395,5,5);
  ellipse(750,410,10,10);
- 
+//glare
+ ellipse(900,220,100,50);
+ ellipse(950,250,10,5);
+//snow 
 
 
 
 
-
+  
 }
 
 function draw() {

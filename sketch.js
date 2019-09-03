881191bea1e9160d578a6b9f73ec3ad19a207cab
mbushn1 <46574392+mbushn1@users.noreply.github.com>
2019-09-03 11:55:23 -0500

    last push

--- sketch.js
+++ sketch.js
@@ -3,6 +3,10 @@ function setup() {
  background(0,0,0);
 //snowglobe 
  ellipse(width/2,height/2,600,600);
+ 
+//ground
+ ellipse(width/2,595,425,175)
+ ellipse(width/2,700,600,100);
 //snowman
  ellipse(750,400,50,50);
  ellipse(750,455,70,70);
@@ -12,8 +16,15 @@ function setup() {
  ellipse(750,410,10,10);
 //glare
  ellipse(900,220,100,50);
- ellipse(950,250,10,5);
-//snow 
+ ellipse(950,250,10,5,75);
+//clouds
+ ellipse(750,275,224,80);
+ ellipse(805,200,200,110);
+ ellipse(682,200,290,101);
+ ellipse(755,250,230,133);
+ ellipse(675,200,270,25);
+
+
 
 
 
